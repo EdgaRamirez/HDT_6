20240133b4b6420f982ea1ee49273385679778a2
EdgaRamirez <ram15236@uvg.edu.gt>
2016-08-24 09:43:42 -0600

    V 1.1
    creacion del proyecto

---
+++ src/main.java
@@ -0,0 +1,17 @@
+
+/**
+ * @author Pareja 9 Hugo Elvira 15249 y Edgar Ramírez 15236
+ * @version 28/08/2016
+ * @Description 
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
